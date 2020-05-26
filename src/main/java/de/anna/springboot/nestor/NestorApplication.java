39e26ba1ede85bc98fb31dc4006299897d1b2e9c
annaGitHubAccount <ann.polanska@gmail.com>
2020-05-26 15:09:07 +0200

    Spring Boot Nestor Initializer

---
+++ src/main/java/de/anna/springboot/nestor/NestorApplication.java
@@ -0,0 +1,13 @@
+package de.anna.springboot.nestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestorApplication.class, args);
+	}
+
+}
